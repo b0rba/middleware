6d99aacb92db2e5bc382887f569ba9b6bd3b592f
b0rba <mlbas@cin.ufpe.br>
2019-10-05 20:45:11 -0300

    feat(marshaller): add marshaller

---
+++ MeuMiddleware/utils/marshaller.go
@@ -0,0 +1,29 @@
+package marshaller
+
+import (
+	"encoding/json"
+	"log"
+	"github.com/b0rba/middleware/MeuMiddleware/utils/miop"
+)
+
+type Marshaller struct{}
+
+func (Marshaller) Marshall(msg miop.Packet) []byte {
+
+	r, err := json.Marshal(msg)
+	if err != nil {
+		log.Fatalf("Marshaller:: Marshall:: %s", err)
+	}
+
+	return r
+}
+
+func (Marshaller) Unmarshall(msg []byte) miop.Packet {
+
+	r := miop.Packet{}
+	err := json.Unmarshal(msg, &r)
+	if err != nil {
+		log.Fatalf("Marshaller:: Unmarshall:: %s", err)
+	}
+	return r
+}
