2794bc3bc39b947b2b454f67c56212cee3b496e9
Borba <mlbas@cin.ufpe.br>
2019-10-05 19:54:24 -0300

    feat(Client&Server): create Client and Server Request Handlers

---
+++ MeuMiddleware/infrastructure/ClientRequestHandler.go
@@ -0,0 +1,59 @@
+package infrastructure
+
+import (
+	"encoding/binary"
+	"log"
+	"net"
+	"strconv"
+)
+
+//Client-2-Server setup struct
+type ClientRequestHandler struct {
+	ServerHost string
+	ServerPort int
+}
+
+func (crh ClientRequestHandler) SendReceive(msg2Server []byte) []byte {
+
+	var conn net.Conn
+	var err error
+	for {
+		conn, err = net.Dial("tcp", "localhost:"+strconv.Itoa(crh.ServerPort))
+		if err == nil {
+			break
+		}
+
+	}
+
+	defer conn.Close()
+
+	// send{ messsage size, message } ; receive {message size, message}
+
+	msg2ServerSize := make([]byte, 4)
+	l := uint32(len(msg2Server))
+	binary.LittleEndian.PutUint32(msg2ServerSize, l)
+	conn.Write(msg2ServerSize)
+	if err != nil {
+		log.Fatalf("CRH:: %s", err)
+	}
+
+	_, err = conn.Write(msg2Server)
+	if err != nil {
+		log.Fatalf("CRH:: %s", err)
+	}
+
+	msgFromServerSize := make([]byte, 4)
+	_, err = conn.Read(msgFromServerSize)
+	if err != nil {
+		log.Fatalf("SRH:: %s", err)
+	}
+	sizeFromServerInt := binary.LittleEndian.Uint32(msgFromServerSize)
+
+	msgFromServer := make([]byte, sizeFromServerInt)
+	_, err = conn.Read(msgFromServer)
+	if err != nil {
+		log.Fatalf("SRH:: %s", err)
+	}
+
+	return msgFromServer
+}
---
+++ MeuMiddleware/infrastructure/ServerRequestHandler.go
@@ -0,0 +1,68 @@
+package infrastructure
+
+import (
+	"encoding/binary"
+	"log"
+	"net"
+	"strconv"
+)
+
+//Server-2-Client setup struct
+type ServerRequestHandler struct {
+	ServerHost string
+	ServerPort int
+}
+
+var ln net.Listener
+var conn net.Conn
+var err error
+
+//send and receive []byte
+
+func (ServerRequestHandler) Send(msg2Client []byte) {
+	//message size, message
+
+	size := make([]byte, 4)
+	l := uint32(len(msg2Client))
+	binary.LittleEndian.PutUint32(size, l)
+	conn.Write(size)
+	if err != nil {
+		log.Fatalf("CRH:: %s", err)
+	}
+
+	_, err = conn.Write(msg2Client)
+	if err != nil {
+		log.Fatalf("CRH:: %s", err)
+	}
+
+	conn.Close()
+	ln.Close()
+}
+
+func (srh ServerRequestHandler) Receive() []byte {
+	//listener, accept connections, message size, message
+
+	ln, err = net.Listen("tcp", srh.ServerHost+":"+strconv.Itoa(srh.ServerPort))
+	if err != nil {
+		log.Fatalf("SRH:: %s", err)
+	}
+
+	conn, err = ln.Accept()
+	if err != nil {
+		log.Fatalf("SRH:: %s", err)
+	}
+
+	size := make([]byte, 4)
+	_, err = conn.Read(size)
+	if err != nil {
+		log.Fatalf("SRH:: %s", err)
+	}
+	sizeInt := binary.LittleEndian.Uint32(size)
+
+	msg := make([]byte, sizeInt)
+	_, err = conn.Read(msg)
+	if err != nil {
+		log.Fatalf("SRH:: %s", err)
+	}
+	return msg
+}
